80ee3d3eb7374eb5a9b6fd604eea15e95b823a55
Raquel Patzdorf <123453220+QuelPatz@users.noreply.github.com>
2023-03-29 14:11:44 +0100

    Update XMLHttpRequest.js

--- XMLHttpRequest.js
+++ XMLHttpRequest.js
@@ -1,4 +1,4 @@
-function loadXMLDoc2() {
+function loadXMLDoc() {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
       if (this.readyState == 4 && this.status == 200) {
